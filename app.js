1dcde99261106a314c49274e87d86bc46826f407
eunwho <eunwho@gmail.com>
2016-08-01 15:57:12 +0900

    connecd to socket server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+//--- mongoose 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/wsnv0');
 
 var db = mongoose.connection;
 
@@ -8,9 +9,14 @@ db.once('open',function(){
 	console.log('Ok db connected');
 });
 
-var wsnSchema = mongoose.Schema({ wsnData: String });
+var wsnSchema = mongoose.Schema({ 
+	wsnData: String,
+	date:{type:Date,default:Date.now}
+});
 
-var wsnDB = mongoose.model('wsnDB',wsnSchema);
+var wsnDB1 = mongoose.model('wsnDB1',wsnSchema);
+
+//--- serial port for xbee
 
 var SerialPort = require('serialport');
 
@@ -24,15 +30,28 @@ port.on('open',function(err){
     console.log('serial open ');
 }); 
 
+//--- socket.io for sending to server fo Wireless Sensor Net 
+
+var io = require('socket.io-client');
+var socket = io.connect('http://192.168.0.119:8080', {reconnect: true});
+
+// Add a connect listener
+socket.on('connect', function (socket) {
+    console.log('Connected!');
+});
+
+
 port.on('data',function (data){
 
-    console.log('RXD: '+data);
+	socket.emit('CH0', 'me', data);
 
-	var wsnIn = new wsnDB({wsnData:data});
+    console.log('RXD  : '+data);
+	
+	var wsnIn = new wsnDB1({wsnData:data});
 	wsnIn.save( function( err, wsnIn ){
 		if(err) return console.error(err);	
 	    console.log('SAVED: '+data);
-	});
+	});	
 });
 
 port.on('error', function(err) {
@@ -41,4 +60,3 @@ port.on('error', function(err) {
 });
 
 console.log('start');
-
